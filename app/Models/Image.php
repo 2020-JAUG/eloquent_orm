5148083f50fbce3daf04fbb5e37dc998fcc08f48
Alejandro Urbina <urbinagonzalezja@gmail.com>
2022-01-13 00:02:39 +0100

    Polimorfismo uno a uno

---
+++ app/Models/Image.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Image extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /** @var string */
+    protected $table = 'images';
+
+    /**
+     * UNO A UNO CON USANDO POLIMORFISMO. ESTÁ TABLA ES UNA TABLA POLIMORFICA
+     * Con este método relaciono las siguientes tablas
+     * (Transoformar Á) ---> Se usará con: user, vídeos, posts
+     * @return void
+     */
+    public function imageable()
+    {
+        return $this->morphTo();
+    }
+}
